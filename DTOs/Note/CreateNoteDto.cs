cf2cab710c85c5f42e779ef0641b238e0a1e12a9
bradmeyn <bradjmeyn@gmail.com>
2026-02-07 15:28:45 +1100

    Update noteDTo

--- DTOs/Note/CreateNoteDto.cs
+++ DTOs/Note/CreateNoteDto.cs
@@ -1,17 +1,23 @@
 using System.Text.Json.Serialization;
-
+using Microsoft.AspNetCore.Http;
 
 namespace CrmApi.DTOs.Note;
 
 public class CreateNoteDto
 {
     [JsonPropertyName("clientId")]
-    public Guid ClientId {get; set;}
+    public Guid ClientId { get; set; }
 
     [JsonPropertyName("type")]
-    public string Type {get; set;} = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    
     [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
+    
     [JsonPropertyName("content")]
     public string Content { get; set; } = string.Empty;
+    
+    // For file uploads
+    [JsonPropertyName("files")]
+    public List<IFormFile>? Files { get; set; }
 }
